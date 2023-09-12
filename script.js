de4e55ac907f3b5ad9ee093713208a719a3bfcfc
unknown <joaovictornomoto@gmail.com>
2023-09-11 22:34:09 -0300

    teste commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("ligth")
+
+ // if(html.classList.contains('ligth')) {
+   // html.classList.remove('ligth')
+   //} else {
+    // html.classList.add('ligth')
+  }
+
