7b860ac18a078b3f0baf0f82739c9e5a4ede4e5a
unknown <joaovictornomoto@gmail.com>
2023-09-27 11:52:05 -0300

    commit

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("ligth")
-  const img = document.squeryselector("#profile img")
-  // if(html.classList.contains('ligth')) {
-  // html.classList.remove('ligth')
-  //} else {
-  // html.classList.add('ligth')
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substtituir a imagem
+  if (html.classList.contains("ligth")) {
+    img.setAttribute("src", "./assets/avatar12.png")
+    // se tiver ligth mode, adicionar imagem ligth
+  } else {
+    // set tiver sem ligth mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
