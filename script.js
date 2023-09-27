98bc2206a436cc5b0edc80eedae008093c6225a1
unknown <joaovictornomoto@gmail.com>
2023-09-26 21:59:44 -0300

    endproject

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("ligth")
-
- // if(html.classList.contains('ligth')) {
-   // html.classList.remove('ligth')
-   //} else {
-    // html.classList.add('ligth')
-  }
-
+  const img = document.squeryselector("#profile img")
+  // if(html.classList.contains('ligth')) {
+  // html.classList.remove('ligth')
+  //} else {
+  // html.classList.add('ligth')
+}
